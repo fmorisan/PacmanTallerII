91a3fed302be1e65c5464855db5edbe8f573ac53
fmorisan <f.morisan@gmail.com>
2016-09-27 10:08:01 -0300

    Agregar metodos y variables de instancia a la clase Personaje.

--- JuegoPacman/src/personajes/Personaje.java
+++ JuegoPacman/src/personajes/Personaje.java
@@ -1,6 +1,28 @@
 package personajes;
 
+import estructuras.Direccion;
+import path.Position;
+
 public abstract class Personaje {
+	
+	private Direccion direccion;
+	// sirve como posicion inicial? podemos asignarle una en el constructor
+	private Position pos; 
+	
+	public Position getPosicion(){
+		return this.pos;
+	}
+	
+	public Position getSiguientePosicion(){
+		//TODO agregar una forma de convertir direcciones a vectores Position para poder sumar
+		return this.pos + this.direccion; 
+	}
+	
+	public void actualizarPosicion(){
+		//TODO agregar una forma de convertir direcciones a vectores Position para poder sumar
+		this.pos += this.direccion;
+	}
+	
 
 	public abstract void crearPersonaje();
 	
