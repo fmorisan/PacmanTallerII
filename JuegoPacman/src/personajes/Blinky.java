a3cb1e59861545c3d377ca8184bed3f0535fe66e
fmorisan <f.morisan@gmail.com>
2016-10-07 09:52:26 -0300

    Agrega estrategiaPersecucion a Blinky

--- JuegoPacman/src/personajes/Blinky.java
+++ JuegoPacman/src/personajes/Blinky.java
@@ -1,23 +1,23 @@
 package personajes;
 
+import estructuras.Direccion;
+import path.Path.Step;
+import path.PathFinder;
 import path.Position;
 
 public class Blinky extends Fantasma{
 	
 	public Blinky(Position posInicial){
 		super(posInicial);
+		// TODO cambiar por un valor seteable en opciones
+		this.esquinaDesignada = new Position(0, 0);
 	}
 
 	@Override
 	public void estrategiaPersecucion(Position posicionPacman) {
 		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void estrategiaAsustado(Position poscicionPacman) {
-		// TODO Auto-generated method stub
-		
+		Step siguientePaso = PathFinder.findPath(this.getPosicion().getX(), this.getPosicion().getY(), posicionPacman.getX(), posicionPacman.getY()).getStep(0);
+		this.setDireccion(Direccion.fromVector(new Position(siguientePaso.getX() - this.getPosicion().getX(), siguientePaso.getY() - this.getPosicion().getY())));
 	}
 	
 	
