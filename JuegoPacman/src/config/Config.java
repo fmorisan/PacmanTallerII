2851d0409cace02d72264656586463d0d3ac9c2f
fmorisan <f.morisan@gmail.com>
2016-10-07 11:59:22 -0300

    Agregar valores por defecto a la clase Config

--- JuegoPacman/src/config/Config.java
+++ JuegoPacman/src/config/Config.java
@@ -3,11 +3,11 @@ package config;
 import path.Position;
 
 public final class Config {
-	public final Position PACMAN_START;
-	public final Position INKY_START;
-	public final Position PINKY_START;
-	public final Position BLINKY_START;
-	public final Position CLYDE_START;
-	public final int GHOST_SPEED;
-	public final int PACMAN_SPEED;
+	public static final Position PACMAN_START = new Position(20, 13);
+	public static final Position INKY_START = new Position(11,	11);
+	public static final Position PINKY_START = new Position(11, 13);
+	public static final Position BLINKY_START = new Position(8, 13);
+	public static final Position CLYDE_START =  new Position(11, 15);
+	public static final int GHOST_SPEED = 10;
+	public static final int PACMAN_SPEED = 10;
 }
