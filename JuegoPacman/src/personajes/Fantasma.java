6001a482bdeb7777eba32c6ed4827f89acc6f704
fmorisan <f.morisan@gmail.com>
2016-10-07 09:42:17 -0300

    Agrega logica de Dispersion a los Fantasmas

--- JuegoPacman/src/personajes/Fantasma.java
+++ JuegoPacman/src/personajes/Fantasma.java
@@ -1,11 +1,17 @@
 package personajes;
 
+import path.Path;
+import path.Path.Step;
+import path.PathFinder;
 import path.Position;
+import estructuras.Direccion;
 import estructuras.Modo;
 
 public abstract class Fantasma extends Personaje {
 
 	 private Modo modo = Modo.PERSECUCION;
+	 
+	 protected Position esquinaDesignada;
 
 	 //private boolean inHome;
 	 //private boolean exitingHome;
@@ -33,5 +39,11 @@ public abstract class Fantasma extends Personaje {
 	public void estrategiaDispersion(){
 		// TODO agregar logica para que los fantasmas vayan a su esquina designada
 		// es igual para todos los fantasmas asi que vale dejarla implementada aca
+		Step siguientePaso = PathFinder.findPath(this.getPosicion().getX(), this.getPosicion().getY(), this.esquinaDesignada.getX(), this.esquinaDesignada.getY()).getStep(0);
+		int dx, dy;
+		dx = siguientePaso.getX() - this.getPosicion().getX();
+		dy = siguientePaso.getY() - this.getPosicion().getY();
+		
+		this.setDireccion(Direccion.fromVector(new Position(dx, dy)));
 	}
 }
