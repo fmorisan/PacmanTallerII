925c4e5f3128d044bcf40231b2caf9cc06db25a0
fmorisan <f.morisan@gmail.com>
2016-10-06 21:31:56 -0300

    Eliminar variables innecesarias en Fantasma.java

--- JuegoPacman/src/personajes/Fantasma.java
+++ JuegoPacman/src/personajes/Fantasma.java
@@ -5,16 +5,11 @@ import estructuras.Modo;
 
 public abstract class Fantasma extends Personaje {
 
-
-	 private static final int VELOCIDAD = 15;
-	 private static final int[] DIRECCION_REVERSA = new int[4];
 	 private Modo modo = Modo.PERSECUCION;
-	
-	 private Position pos;
 
-	 private boolean inHome;
-	 private boolean exitingHome;
-	 private boolean enteringHome;
+	 //private boolean inHome;
+	 //private boolean exitingHome;
+	 //private boolean enteringHome;
 	 
 	 protected Fantasma(Position posInicial){
 		 super(posInicial);
