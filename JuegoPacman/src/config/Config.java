82d498fd71f572ba617972b52ef82e636f8eb928
fmorisan <f.morisan@gmail.com>
2016-10-07 11:55:00 -0300

    Utilizar Config.java para definir variables usadas por las demas clases

--- JuegoPacman/src/config/Config.java
+++ JuegoPacman/src/config/Config.java
@@ -1,5 +1,13 @@
 package config;
 
-public final class Config {
+import path.Position;
 
+public final class Config {
+	public final Position PACMAN_START;
+	public final Position INKY_START;
+	public final Position PINKY_START;
+	public final Position BLINKY_START;
+	public final Position CLYDE_START;
+	public final int GHOST_SPEED;
+	public final int PACMAN_SPEED;
 }
