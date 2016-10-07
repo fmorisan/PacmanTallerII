766cd09d0c4eb140508a99988d8951f48009de2c
fmorisan <f.morisan@gmail.com>
2016-10-07 10:06:49 -0300

    AGregar estrategiaPersecucion a Pinky

--- JuegoPacman/src/personajes/Pinky.java
+++ JuegoPacman/src/personajes/Pinky.java
@@ -1,26 +1,37 @@
 package personajes;
 
+import estructuras.Direccion;
+import path.Path.Step;
+import path.PathFinder;
 import path.Position;
 
 public class Pinky extends Fantasma{
 	
+	private Position posicionAnteriorPacman = null;
+	
 	public Pinky(Position posInicial){
 		super(posInicial);
-	}
-	
-	public void estrategia(Position posicionPacman){
-		
 	}
 
 	@Override
 	public void estrategiaPersecucion(Position posicionPacman) {
 		// TODO Auto-generated method stub
-		
+		if (posicionAnteriorPacman != null){
+			Position vectorPacman = new Position(posicionPacman.getX() - posicionAnteriorPacman.getX(), posicionPacman.getY() - posicionAnteriorPacman.getY());
+			Step siguientePaso = PathFinder.findPath(
+				this.getPosicion().getX(), posicionPacman.getX()+4*vectorPacman.getX(),
+				this.getPosicion().getY(), posicionPacman.getY()+4*vectorPacman.getY()
+			).getStep(0);
+			
+			this.setDireccion(
+				Direccion.fromVector(new Position(
+					siguientePaso.getX() - this.getPosicion().getX(),
+					siguientePaso.getY() - this.getPosicion().getY()
+					)
+				)
+			);
+		}
+		posicionAnteriorPacman = posicionPacman;
 	}
-
-	@Override
-	public void estrategiaAsustado(Position poscicionPacman) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 }
