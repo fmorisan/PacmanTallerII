446b4e321e980d22b57a1d2375b31019ab5450cb
fmorisan <f.morisan@gmail.com>
2016-10-07 11:46:30 -0300

    Agrega estrategiaPersecucion a Inky y Clyde

--- JuegoPacman/src/personajes/Clyde.java
+++ JuegoPacman/src/personajes/Clyde.java
@@ -1,6 +1,9 @@
 package personajes;
 
+import path.PathFinder;
 import path.Position;
+import estructuras.Direccion;
+import path.Path.Step;
 
 public class Clyde extends Fantasma{
 	
@@ -15,12 +18,19 @@ public class Clyde extends Fantasma{
 	@Override
 	public void estrategiaPersecucion(Position posicionPacman) {
 		// TODO Auto-generated method stub
+		int distanciaAPacman = Math.abs(this.getPosicion().getX() - posicionPacman.getX()) + Math.abs(this.getPosicion().getY() - posicionPacman.getY());
 		
-	}
-
-	@Override
-	public void estrategiaAsustado(Position poscicionPacman) {
-		// TODO Auto-generated method stub
+		if (distanciaAPacman > 8){
+			Step proximoPaso = PathFinder.findPath(this.getPosicion().getX(), this.getPosicion().getY(), posicionPacman.getX(), posicionPacman.getY()).getStep(0);
+			this.setDireccion(Direccion.fromVector(
+					new Position(
+							proximoPaso.getX() - this.getPosicion().getX(),
+							proximoPaso.getY() - this.getPosicion().getY()
+							)
+					));
+		} else {
+			this.estrategiaDispersion();
+		}
 		
 	}
 }
--- JuegoPacman/src/personajes/Inky.java
+++ JuegoPacman/src/personajes/Inky.java
@@ -1,8 +1,12 @@
 package personajes;
 
+import estructuras.Direccion;
+import path.Path.Step;
+import path.PathFinder;
 import path.Position;
 
 public class Inky extends Fantasma{
+	private Position posicionAnteriorPacman = null;
 	
 	public Inky(Position posInicial) {
 		super(posInicial);
@@ -11,13 +15,33 @@ public class Inky extends Fantasma{
 	@Override
 	public void estrategiaPersecucion(Position posicionPacman) {
 		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void estrategiaAsustado(Position poscicionPacman) {
-		// TODO Auto-generated method stub
-		
+		if (posicionAnteriorPacman != null){
+			Position vectorPacman = new Position(
+					posicionPacman.getX() - posicionAnteriorPacman.getX(),
+					posicionPacman.getY() - posicionAnteriorPacman.getY()
+					);
+			// dos casilleros adelante de Pacman
+			Position P1 = Position.add(posicionPacman, vectorPacman);
+			P1 = Position.add(P1, vectorPacman);
+			
+			Position vectorDesplazamiento = new Position(
+					P1.getX() - this.getPosicion().getX(),
+					P1.getY() - this.getPosicion().getY()
+					);
+			
+			Position target = new Position(
+					this.getPosicion().getX()+2*P1.getX(),
+					this.getPosicion().getY()+2*P1.getY()
+					);
+			Step proximoPaso = PathFinder.findPath(this.getPosicion().getX(), this.getPosicion().getY(), target.getX(), target.getY()).getStep(0);
+			
+			this.setDireccion(Direccion.fromVector(
+					new Position(
+							proximoPaso.getX() - this.getPosicion().getX(),
+							proximoPaso.getY() - this.getPosicion().getY()
+							)
+					));
+		}
 	}
 	
 }
