1ce0fe5be425eaf15c1fad1d3d0f51451956c3d7
fmorisan <f.morisan@gmail.com>
2016-10-08 11:58:48 -0300

    Agrega valores y comentarios a Config.java

--- JuegoPacman/src/config/Config.java
+++ JuegoPacman/src/config/Config.java
@@ -3,11 +3,24 @@ package config;
 import path.Position;
 
 public final class Config {
+	/*
+	 * Posiciones iniciales
+	 */
 	public static final Position PACMAN_START = new Position(20, 13);
 	public static final Position INKY_START = new Position(11,	11);
 	public static final Position PINKY_START = new Position(11, 13);
 	public static final Position BLINKY_START = new Position(8, 13);
 	public static final Position CLYDE_START =  new Position(11, 15);
+	
+	/*
+	 * Velocidades
+	 */
 	public static final int GHOST_SPEED = 10;
 	public static final int PACMAN_SPEED = 10;
+	
+	/*
+	 * Puntajes
+	 */
+	public static final int PACPOINT_SCORE = 10;
+	public static final int POWERBALL_SCORE = 50;
 }
