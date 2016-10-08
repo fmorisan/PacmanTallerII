40475037acddaee60e192d3c2c24dc4e56dcddf9
fmorisan <f.morisan@gmail.com>
2016-10-08 11:58:33 -0300

    Agrega Tunel a pacman.objetos
    Tunel es un objeto del mapa que permite a PacMan _teletransportarse_ a
    otra parte del mapa.

---
+++ JuegoPacman/src/objetos/Tunel.java
@@ -0,0 +1,19 @@
+package objetos;
+
+import path.Position;
+
+public class Tunel {
+	private Position posicionSalida;
+
+	public Tunel(Position posicionSalida) {
+		super();
+		this.posicionSalida = posicionSalida;
+	}
+
+	public Position getPosicionSalida() {
+		return posicionSalida;
+	}
+	
+	
+
+}
